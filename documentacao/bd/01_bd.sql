9e6daa2d92553ec9cd8cfc89e9faecacd234a00f
Érica Almeida <erikamanaia@hotmail.com>
2025-04-08 14:44:50 -0300

    Update 01_bd.sql

--- documentacao/bd/01_bd.sql
+++ documentacao/bd/01_bd.sql
@@ -3,25 +3,25 @@ create database sistema_de_hotel;
 use sistema_de_hotel;
 
 create table hospede(
-id int auto_increment primary key,
-nome varchar(100) not null,
-cpf int not null, 
-telefone int, 
-email varchar(255) not null
+    id int primary key auto_increment,
+    nome varchar(100),
+    cpf int, 
+    telefone int, 
+    email varchar(255)
 );
 
 create table quarto(
-id int auto_increment primary key,
-numero_do_quarto int not null,
-tipo_do_quarto text not null,
-valor_do_quarto int not null
+    id int primary key auto_increment,
+    numero_do_quarto int,
+    tipo_do_quarto text,
+    valor_do_quarto int
 );
 
 create table reserva(
-id int auto_increment primary key,
-nome_hospede varchar(100) not null,
-numero_quarto int not null,
-data_check_in varchar(10) not null,
-data_check_out varchar(10) not null
+    id int primary key auto_increment,
+    nome_hospede varchar(100),
+    numero_quarto int,
+    data_check_in varchar(10),
+    data_check_out varchar(10)
 );
 
