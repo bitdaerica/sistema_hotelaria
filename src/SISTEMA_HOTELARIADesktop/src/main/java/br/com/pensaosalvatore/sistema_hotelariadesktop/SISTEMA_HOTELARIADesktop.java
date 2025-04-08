31ef3fef506d8c6d8356b79427e7e6a5d4ed761f
erikamanaia <erikamanaia@hotmail.com>
2025-04-08 14:25:57 -0300

    atualização diretorio src

---
+++ src/SISTEMA_HOTELARIADesktop/src/main/java/br/com/pensaosalvatore/sistema_hotelariadesktop/SISTEMA_HOTELARIADesktop.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.pensaosalvatore.sistema_hotelariadesktop;
+
+/**
+ *
+ * @author Érica_Almeida
+ */
+public class SISTEMA_HOTELARIADesktop {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/SISTEMA_HOTELARIAModelo/src/main/java/br/com/pensaosalvatore/sistema_hotelariamodelo/SISTEMA_HOTELARIAModelo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package br.com.pensaosalvatore.sistema_hotelariamodelo;
+
+/**
+ *
+ * @author Érica_Almeida
+ */
+public class SISTEMA_HOTELARIAModelo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
