8120ea7ae4e79e57f55e1f8fa7764618978a56b3
Érica Almeida <erikamanaia@hotmail.com>
2025-05-06 23:10:07 -0300

    Atualizar o hotel.sql

--- documentacao/bd/hotel.sql
+++ documentacao/bd/hotel.sql
@@ -4,32 +4,55 @@ default collate utf8_general_ci;
 
 use hotel;
 
-create table hospede(
-    id int primary key auto_increment,
+create table pessoa(
     nome varchar(40) not null,
     sobrenome varchar(60) not null,
+    dataNascimento date,
+    sexo varchar(10) not null,
     cpf varchar(11) not null unique,
     rg varchar(20) not null,
-    sexo varchar(10),
-    celular varchar(11) not null,
+    observacao text(255),
+    celular varchar(13) not null,
     fixo varchar (11), 
     email varchar(255) not null unique,
-    dataNascimento date,
-    dataCadastro date,
+    dataCadastro date
+);
+
+
+create table hospede(
+    id int primary key auto_increment,
     ultimaVisita date,
     nacionalidade varchar(50),
-    observacao text(255),
     preferencias text(255),
-    metodo_contato_preferido varchar(50),
+    metodo_contato_preferido varchar(50)
+    );
+
+
+create table funcionario(
+    id int primary key auto_increment,
+    funcao varchar(30) not null,
+    nome_mae varchar(100) not null,
+    nome_pai varchar(100),
+    data_admissao date,
+    status_funcionario varchar(10),
+    tipo_contrato varchar(12),
+    data_saida date,
+    foto varchar(255),
+    usuario varchar(40) unique,
+    senha varchar(40),
+    acesso varchar(20)
+);
+
+create table endereço(
+    id int primary Key auto_increment,
     rua varchar(255),
     numero varchar(40),
     complemento varchar(40),
     bairro varchar(40),
     cidade varchar(60) not null,
     estado varchar(2) not null,
-    cep varchar(20) not null
+    cep varchar(20) not null,
 );
-
 
 create table quarto(
     id int primary key auto_increment,
@@ -64,34 +87,3 @@ create table reserva(
     data_check_out varchar(10)
 );
 
-create table funcionario(
-    id int primary key auto_increment,
-    nome varchar(40) not null,
-    sobrenome varchar(60) not null,
-    cpf varchar(11) not null unique,
-    rg varchar(20) not null,
-    sexo varchar(10) not null,
-    funcao varchar(30) not null,
-    nome_mae varchar(100) not null,
-    nome_pai varchar(100),
-    celular varchar(11) not null,
-    fixo varchar (11), 
-    email varchar(255) not null unique,
-    dataNascimento date,
-    data_admissao date,
-    status_funcionario varchar(10),
-    tipo_contrato varchar(12),
-    data_saida date,
-    observacao text(255),
-    rua varchar(255),
-    numero varchar(40),
-    complemento varchar(40),
-    bairro varchar(40),
-    cidade varchar(60) not null,
-    estado varchar(2) not null,
-    cep varchar(20) not null,
-    foto varchar(255),
-    usuario varchar(40) unique,
-    senha varchar(40),
-    acesso varchar(20)
-);
