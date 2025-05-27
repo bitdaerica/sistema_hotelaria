0c0af7ed241db620d692277614976dca8f63f19a
bitdaerica <erikamanaia@hotmail.com>
2025-05-27 14:20:44 -0300

    help

--- src/SISTEMAHOTELARIAModelo/src/main/java/br/com/pensaosalvatore/sistema_hotelaria/modelo/dto/enumeradores/PreferenciaQuarto.java
+++
@@ -1,9 +0,0 @@
-package br.com.pensaosalvatore.sistema_hotelaria.modelo.dto.enumeradores;
-
-/**
- *
- * @author Érica_Almeida
- */
-public enum PreferenciaQuarto {
-    SOLTEIRO, DUPLO, TRIPLO, QUÁDRUPLO, SUITE, DELUXE, EXECUTIVO, ADAPTADO, LUXO, FAMILIAR
-}
